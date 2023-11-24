8c642769405f7ee3f0c346e48292d824ddda513a
Isaiahcodes-3321 <shellisaiah2020@gmail.com>
2023-11-24 14:00:58 +0100

    done

--- noteapp/lib/Screens/Api_Service/saveNoteservice.dart
+++ noteapp/lib/Screens/Api_Service/saveNoteservice.dart
@@ -1,3 +1,4 @@
+import '../HomePage/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:noteapp/Screens/Api_Service/export.dart';
@@ -5,7 +6,9 @@ import 'package:noteapp/Screens/Api_Service/export.dart';
 class SaveNoteService {
   ApiServiceState apiServiceState = ApiServiceState();
 //save note
-  Future<void> saveUserNote(BuildContext context, ) async {
+  Future<void> saveUserNote(
+    BuildContext context,
+  ) async {
     showDialog(
       context: context,
       builder: (context) {
@@ -13,7 +16,6 @@ class SaveNoteService {
       },
     );
 
-   
     String noteBodyValue = GlobalControllers.providerRef
         .read(UserNewNotes.noteBody.notifier)
         .state;
@@ -21,76 +23,81 @@ class SaveNoteService {
         .read(UserNewNotes.noteTittle.notifier)
         .state;
 
-     GlobalControllers.tokenKey = await Hive.openBox('tokenBox');
+    GlobalControllers.tokenKey = await Hive.openBox('tokenBox');
     final tokenStorage = GlobalControllers.tokenKey.getAt(0) as TokenStorage;
 
+    print('saving note to storage');
+    final imageFile = logics.image;
+    String imagePath = imageFile?.path ?? '';
 
-   print('saving note to storage');
-final imageFile = logics.image;
-String imagePath = imageFile?.path ?? '';
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse(apiServiceState.createNoteEndpoint),
+      );
 
-try {
-  var request = http.MultipartRequest(
-    'POST',
-    Uri.parse(apiServiceState.createNoteEndpoint),
-  );
+      // Add headers
+      request.headers['authorization'] = tokenStorage.myToken;
 
-  // Add headers
-  request.headers['authorization'] = tokenStorage.myToken;
+      // Add form-data fields
+      request.fields['title'] = noteTittleValue;
+      request.fields['note'] = noteBodyValue;
 
-  // Add form-data fields
-  request.fields['title'] = noteTittleValue;
-  request.fields['note'] = noteBodyValue;
+      // Add image file
+      if (imageFile != null) {
+        request.files.add(
+          await http.MultipartFile.fromPath('image', imageFile.path),
+        );
+      }
 
-  // Add image file
-  if (imageFile != null) {
-    request.files.add(
-      await http.MultipartFile.fromPath('image', imageFile.path),
-    );
-  }
+      var response = await http.Response.fromStream(await request.send());
 
-  var response = await http.Response.fromStream(await request.send());
-
-  if (response.statusCode == 200 || response.statusCode == 201) {
-    // Handle success
-    // ignore: use_build_context_synchronously
-    Navigator.pop(context);
-    // ignore: use_build_context_synchronously
-    ReusedSnackBar.showCustomSnackBar(
-      context,
-      "Note Saved Successfully ",
-      themeColor,
-      const Duration(seconds: 4),
-    );
-  } else if (response.statusCode == 404) {
-    // Handle 404 error
-    // ignore: use_build_context_synchronously
-    Navigator.pop(context);
-    // ignore: use_build_context_synchronously
-    ReusedSnackBar.showCustomSnackBar(
-      context,
-      "Failed to Save: Please try again.",
-      themeColor,
-      const Duration(seconds: 4),
-    );
-  } else {
-    // Handle other errors
-    // ignore: use_build_context_synchronously
-    Navigator.pop(context);
-    // ignore: use_build_context_synchronously
-    ReusedSnackBar.showCustomSnackBar(
-      context,
-      "Failed to Save note: Please try again.",
-      themeColor,
-      const Duration(seconds: 4),
-    );
-    print("Error: Unexpected error occurred - ${response.statusCode}");
-    print("\n Response data: ${response.body}");
-  }
-} catch (e) {
-  // Handle exceptions
-  print("Error: $e");
-}
-
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        // Handle success
+        // ignore: use_build_context_synchronously
+        Navigator.pop(context);
+        // ignore: use_build_context_synchronously
+        Navigator.push<void>(
+          context,
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => const HomePage(),
+          ),
+        );
+        // ignore: use_build_context_synchronously
+        ReusedSnackBar.showCustomSnackBar(
+          context,
+          "Note Saved Successfully ",
+          themeColor,
+          const Duration(seconds: 4),
+        );
+      } else if (response.statusCode == 404) {
+        // Handle 404 error
+        // ignore: use_build_context_synchronously
+        Navigator.pop(context);
+        // ignore: use_build_context_synchronously
+        ReusedSnackBar.showCustomSnackBar(
+          context,
+          "Failed to Save: Please try again.",
+          themeColor,
+          const Duration(seconds: 4),
+        );
+      } else {
+        // Handle other errors
+        // ignore: use_build_context_synchronously
+        Navigator.pop(context);
+        // ignore: use_build_context_synchronously
+        ReusedSnackBar.showCustomSnackBar(
+          context,
+          "Failed to Save note: Please try again.",
+          themeColor,
+          const Duration(seconds: 4),
+        );
+        print("Error: Unexpected error occurred - ${response.statusCode}");
+        print("\n Response data: ${response.body}");
+      }
+    } catch (e) {
+      // Handle exceptions
+      print("Error: $e");
+    }
   }
 }
